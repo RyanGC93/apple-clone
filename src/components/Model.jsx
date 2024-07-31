606077c6ffda564de2e882a35b1f6a8194661c1b
Ryan Conk <65003807+RyanGC93@users.noreply.github.com>
2024-07-31 15:54:44 +0000

    fix import error

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
-import { View } from "";
+import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
 import { animateWithGsapTimeline } from "../utils/animations";
 
