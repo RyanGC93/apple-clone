b9251c36d6f3096c96a421166ffb6339f733390a
Ryan Conk <65003807+RyanGC93@users.noreply.github.com>
2024-08-01 00:17:25 +0000

    add the features component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
+import Feature from "./components/Feature";
 export default function App() {
   return (
     <main className="bg-black">
@@ -9,6 +10,7 @@ export default function App() {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   );
 }
