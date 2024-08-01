f95c5fb176147b81c4b43e567ff79ed3ecc7f895
Ryan Conk <65003807+RyanGC93@users.noreply.github.com>
2024-08-01 00:21:36 +0000

    fix bug - Features naming issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
-import Feature from "./components/Feature";
+import Features from "./components/Features";
 export default function App() {
   return (
     <main className="bg-black">
